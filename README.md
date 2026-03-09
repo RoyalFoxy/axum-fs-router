2217225fb4fd2b202baca7472846de7a8d34c3da
RoyalFoxy <sepp.toni.mo@gmail.com>
2026-03-09 22:42:59 +0900

    Add documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Axum Filesystem Router
+
+This crate can be used to have a filesystem router.
+
+> **NOTE:** This is an early version of the project.
+
+Look at the doc comments of the macros to know what they do.
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use crate::segmets::Routes;
+use crate::routes::Routes;
 use quote::quote;
 
-mod segmets;
+mod routes;
 
 const ROUTES_FOLDER: &str = "./src/routes";
 
+/// An internal wrapper function to return a [`Result`]
 fn traverse_routes_wrapper() -> eyre::Result<proc_macro2::TokenStream> {
     let routes = Routes::new(ROUTES_FOLDER)?;
 
@@ -14,6 +15,22 @@ fn traverse_routes_wrapper() -> eyre::Result<proc_macro2::TokenStream> {
     })
 }
 
+/// This macro will traverse the `src/routes` folder in your project and search for files any of these names:
+///
+/// - `any.rs`
+/// - `connect.rs`
+/// - `delete.rs`
+/// - `get.rs`
+/// - `head.rs`
+/// - `options.rs`
+/// - `patch.rs`
+/// - `post.rs`
+/// - `put.rs`
+/// - `trace.rs`
+///
+/// If a file is found recursively, a module will be created for it.
+///
+/// > **NOTE:** You should put the [`traverse_routes`] invocation into the root of the project and into a seperate file. Due to some glitchiness with rust-analyzer it will alter the code if a file under the routes folder is renamed.
 #[proc_macro]
 pub fn traverse_routes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
     match traverse_routes_wrapper() {
@@ -26,6 +43,7 @@ pub fn traverse_routes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
     .into()
 }
 
+/// An internal wrapper function to return a [`Result`]
 fn router_wrapper() -> eyre::Result<proc_macro2::TokenStream> {
     let router = Routes::new(ROUTES_FOLDER)?.to_router();
 
@@ -38,6 +56,9 @@ fn router_wrapper() -> eyre::Result<proc_macro2::TokenStream> {
     })
 }
 
+/// This macro returns an axum `Router` that you can use however you want. You can merge it with an existing router or just use it as a base.
+///
+/// > **NOTE:** This macro expects a `pub async fn handler()` to be exported from each of the handler files. It needs to be async as thats what axum wants. You can use any extractors you would normally use in an axum handler.
 #[proc_macro]
 pub fn router(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
     match router_wrapper() {
